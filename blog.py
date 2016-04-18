081d93780a8fa59b1405ee54a7af410108dff917
Neo0 <wjh100@live.cn>
2016-04-18 17:44:18 +0800

    simple version

--- blog.py
+++ blog.py
@@ -1,13 +1,27 @@
-from flask import Flask
+# coding:utf-8
+import sqlite3
+import config
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
 
+
+# create little application
 app = Flask(__name__)
+app.config.from_pyfile('../config.py')
 
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
 def index_main():
     return 'aloha'
     
-    
-    
+     
+@app.route('/post/<pid>')
+def get_post(pid):
+    pass
+
+  
 if __name__ == '__main__':
-    app.run(port=4000)
+    app.run(debug=True,port=4000)
---
+++ config.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+
+
+# configuration
+DATABASE = '/tmp/flask_blog.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'root'
+PASSWORD = 'toor'
